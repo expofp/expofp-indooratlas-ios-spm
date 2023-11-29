f492f3487db1edf3e85980f0e3549543c2c9b518
Vladimir <vladimir@expofp.com>
2023-11-29 14:52:52 +0300

    Release 4.2.16

---
+++ ExpoFpIndoorAtlas.xcframework/ios-arm64_x86_64-simulator/ExpoFpIndoorAtlas.framework/Headers/ExpoFpIndoorAtlas.h
@@ -0,0 +1,18 @@
+//
+//  ExpoFpIndoorAtlas.h
+//  ExpoFpIndoorAtlas
+//
+//  Created by Vladimir Nikitin on 11/23/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExpoFpIndoorAtlas.
+FOUNDATION_EXPORT double ExpoFpIndoorAtlasVersionNumber;
+
+//! Project version string for ExpoFpIndoorAtlas.
+FOUNDATION_EXPORT const unsigned char ExpoFpIndoorAtlasVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExpoFpIndoorAtlas/PublicHeader.h>
+
+
---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ExpoFpIndoorAtlas",
+    platforms: [.iOS(.v14)],
+
+    products: [
+        .library(
+            name: "ExpoFpIndoorAtlas",
+            targets: ["ExpoFpIndoorAtlasTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.2.16"),
+    	.package(url: "https://github.com/IndoorAtlas/ios-spm", branch: "main"),
+
+    ],
+    targets: [
+        .target(
+            name: "ExpoFpIndoorAtlasTarget",
+            dependencies: [
+                .product(name: "ExpoFpCommon", package: "expofp-common-ios-spm"),
+                .product(name: "IndoorAtlas", package: "ios-spm"),
+                .target(name: "ExpoFpIndoorAtlas")
+            
+            ], path: "ExpoFpIndoorAtlasTarget"),
+
+        .binaryTarget(name: "ExpoFpIndoorAtlas",
+                      path: "ExpoFpIndoorAtlas.xcframework"
+                     ),
+
+    ],
+    swiftLanguageVersions: [.v5]
+)
