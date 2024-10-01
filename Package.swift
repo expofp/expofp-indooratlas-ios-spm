57e3e651701c611a796ce4956d31d430a4516388
vladimir-n0691 <vladimir@expofp.com>
2024-10-01 19:20:13 +0300

    Release 4.5.9

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ExpoFpIndoorAtlasTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.5.6"),
+        .package(url: "https://github.com/expofp/expofp-common-ios-spm", exact: "4.5.9"),
     	.package(url: "https://github.com/IndoorAtlas/ios-spm", from: "3.6.7"),
 
     ],
